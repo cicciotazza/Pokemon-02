8dc36478b013afcdb112162905f3d96d9376cdda
cicciotazza <69942262+cicciotazza@users.noreply.github.com>
2021-11-10 11:07:17 +0100

    Array created
    Added five objects to the array with three keys for each one

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pokemons
+## Basic JS, objects and array
+
+Task02
+*Create a new variable called pokemonList*
+*Assign it a blank array*
+*Add at least these three keys: name - height - types*
+*Save, commit, push*
+[x]
+
+
+Pokemons list used
+[http](https://www.pokemon.com/us/pokedex/)
---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Pokemon list v1.0
+let pokemonList = [
+{
+    name: 'Paras',              //used short names to avoid mistakes
+    height: 0.3,
+    type: ['grass', 'bug']
+},
+{
+    name: 'Rattata',
+    height: 0.3,
+    type: ['normal']
+},
+{
+    name: 'Zubat',
+    height: 0.8,
+    type: ['poison', 'flying']
+},
+{
+    name: 'Oddish',
+    height: 0.5,
+    type: ['grass', 'poison']
+},
+{
+    name: 'Magnezone',
+    height: 1.2,
+    type: ['electric', 'steel']
+}
+];
