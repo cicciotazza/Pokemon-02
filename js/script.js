5dce3830b072740363235da4d5261a62ca2ee2ac
cicciotazza <69942262+cicciotazza@users.noreply.github.com>
2021-11-25 16:36:04 +0100

    polyfills, API, promise, JSON
    Implented a list ok Pokemon through an external url source, loaded useing an API and written a function and eachFor using json too

--- js/script.js
+++ js/script.js
@@ -1,72 +1,85 @@
-//Task 1.6
-alert("Click here to chek your Pokemon");
+alert("WlV 1.7 - Click here to chek your Pokemon");
 
 let pokemonRepository = (function () {                              //IIFE starts function with an Array
-	let pokemonList = [
-    {
-        name: 'Paras',
-        height: 0.3,
-        type: ['grass', 'bug']
-    },
-    {
-        name: 'Rattata',
-        height: 0.3,
-        type: ['normal']
-    },
-    {
-        name: 'Zubat',
-        height: 0.8,
-        type: ['poison', 'flying']
-    },
-    {
-        name: 'Oddish',
-        height: 0.5,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Magnezone',
-        height: 1.2,
-        type: ['electric', 'steel']
-    }	
-];
+    let pokemonList = [];
+    let apiUrl ='https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-function add(pokemon) {                                         //check whether of not the iem is a an objet
-    if (typeof pokemon === 'object') {
-        pokemonList.push(pokemon);                              //adds the new item
-    } else {
-      console.log('This pokemon is not an object!');            //false condition
+    function add(pokemon) {                                         //check whether or not the iem is a an objet
+        if 
+            (typeof pokemon === "object" &&                          //check if the type of Pokemon has name and detail
+                "name" in pokemon &&
+                "detailsUrl" in pokemon
+        ) {
+            pokemonList.push(pokemon);                              //if true adds the new item pushing into the repo
+        } else {
+        console.log("Uncorrect - This pokemon is not an object!");  //false writes condition
+        }
     }
-  }
-function getAll() {                                             //Gives back pokemonList array
-    return pokemonList;
-}
-function addListItem(pokemon){          
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");   //returns a Pokemon in each iteration
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");           //CSS class for the button
-    listpokemon.appendChild(button);                //children to append
-    pokemonList.appendChild(listpokemon);           
-    button.addEventListener('click', function () {  //event listner 
-        showDetails(pokemon);
-    });
+    function getAll() {                                             //Gives back pokemonList array
+        return pokemonList;                                         
     }
+    function addListItem(pokemon){          
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");           //returns a Pokemon in each iteration
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");                    //CSS class for the button
+        listpokemon.appendChild(button);                         //children to append
+        pokemonList.appendChild(listpokemon);   
+        button.addEventListener('click', function (event) {      //if the "click" happens it will report the specific Pokemon
+            showDetails(pokemon);                             
+        });                                                   
+        }                            
+        
+        function loadList () {                                  //load details function 
+            return fetch(apiUrl).then(function (response) {     //promise, fetch the API with the url above
+                return response.json();
+            }).then(function (json) {
+                json.results.forEach(function (item) {          //forEach loop, "json" is the array, the "results" come from the url
+                    let pokemon = {                             //pokenon variable into an object with two keys
+                        name: item.name,
+                        detailsUrl: item.url 
+                    };
+                    add(pokemon);                               //comes from the function add(pokemon)
+                    console.log(pokemon);
+                });
+            }).catch(function (e) {
+                console.error(e);
+            })
+        }
+        
+        function loadDetails(item) {                                //the function takes a parameter which is the "item"
+            let url = item.detailsUrl;                              //each url link for each Pokemon
+            return fetch(url).then(function (response) {            
+                return response.json();                             //it returns the details from each one of them
+            }).then(function (details) {
+                item.imageUrl = details.sprites.front_default;      //details added to the item, "sprites" belongs to the url
+                item.height = details.height;
+                item.types = details.types;                         //seatching for types which is actually an array
+            }).catch(function (e) {
+              console.error(e);
+            });
+        }
 
-function showDetails(pokemon) {                     //get the information once clicked
-    console.log(pokemon);
-}
-  
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem
-  };
-})();
-
-pokemonRepository.add({ name: 'Weddle', height: 1.1, types: ['bug', 'poison'] });
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-pokemonRepository.addListItem(pokemon);
-});
+        function showDetails(item) {
+            pokemonRepository.loadDetails(item).then(function() 
+            {
+                console.log(item);                                  //print a specific pokemon based on the funtion list
+            });
+        }    
+        
+        return {
+            add: add,
+            getAll: getAll,
+            addListItem: addListItem,
+            loadList: loadList,
+            loadDetails: loadDetails,
+            showDetails: showDetails
+          };
+        })();
+        
+        pokemonRepository.loadList().then(function () {             //display the Pokemon with button and name
+          pokemonRepository.getAll().forEach(function (pokemon) {
+            pokemonRepository.addListItem(pokemon);
+          });
+        });
